39c74a308eca04d64fd191b3cc9693cc6b5875a9
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-11-13 07:46:52 +0300

    feat: add solution count-vowels tasc

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -315,8 +315,17 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+  let count = 0;
+
+  str.split('').forEach((value) => {
+    if (vowels.includes(value)) {
+      count += 1;
+    }
+  });
+
+  return count;
 }
 
 /**
